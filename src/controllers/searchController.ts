23efaef058820e71ca94e6120f65a8e169cddb79
daanborges <daniellborgess97@outlook.com>
2025-03-06 14:23:49 -0300

    final project 1.0

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,5 +1,21 @@
 import { Request, Response } from "express";
 
-const search = (req: Request, res: Response) => {
-  // res.render('pages/search')
+import { Pet } from "../models/pet";
+import { CreateMenuObeject } from "../helpers/createMenuObject";
+
+export const search = (req: Request, res: Response) => {
+  let query: string = req.query.q as string;
+
+  if (!query) {
+    res.redirect("/");
+    return;
+  }
+
+  let list = Pet.getFromName(query);
+
+  res.render("pages/page", {
+    menu: CreateMenuObeject(""),
+    list,
+    query,
+  });
 };
--- src/server.ts
+++ src/server.ts
@@ -18,7 +18,7 @@ server.use(expres.static(path.join(__dirname, "../public")));
 server.use(mainRoutes);
 
 server.use((req, res) => {
-  res.send("Página não encontrada");
+  res.render("pages/404");
 });
 
 server.listen(process.env.PORT);
